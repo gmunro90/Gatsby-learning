5d69c0c07875fc7bf411455448a64680294edff3
Greg Munro <gregmunro90@gmail.com>
2022-03-16 15:31:11 +0100

    remove link for a tag

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,16 @@
 import React from "react"
-import { Link } from 'gatsby'
 
 const ContactPage = () => {
   return (
     <div>
       <h1>Contact Page</h1>
-      <p>Email me @ gregmunro90@gmail.com</p>
-      <p>Reach me on twitter <Link to="https://twitter.com/momothelocal">here</Link></p>
-
+      <p>Email me: gregmunro90@gmail.com</p>
+      <p>
+        Reach me on twitter{" "}
+        <a href="https://twitter.com/momothelocal" target="_blank">
+          @momothelocal
+        </a>
+      </p>
     </div>
   )
 }
